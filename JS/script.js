78120cfa01b2ddec04f7e6946bc3dc35468b40f2
carlomarcon <carlomarcon1998@gmail.com>
2023-10-18 14:52:59 +0200

    ADD: JS SOLUTION

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+let Distance = parseInt(prompt("Quanti km desidera percorrere ?"));
+
+let Age = parseInt(prompt("Qual'è la sua età ?"));
+
+let normalPrice = Distance * 0.21;
+
+if (Age < 18) {
+  let under18Price = normalPrice - normalPrice * 0.2;
+  document.getElementById("price").innerHTML =
+    under18Price.toFixed(2) + " €, hai ottenuto lo sconto del 20%";
+} else if (Age > 65) {
+  let over65Price = normalPrice - normalPrice * 0.4;
+  document.getElementById("price").innerHTML =
+    over65Price.toFixed(2) + " €, hai ottenuto lo sconto del 40%";
+} else {
+  document.getElementById("price").innerHTML =
+    normalPrice.toFixed(2) +
+    " €,non sono previsti sconti per la tua fascia d'età.";
+}
+
+if (isNaN(normalPrice)) {
+  alert("Solo numeri !!!!!");
+  document.getElementById("price").innerHTML = "nullo, meglio tu vada a piedi.";
+}
